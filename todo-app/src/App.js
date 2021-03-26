f092dcafd5a35456338668d75c2b3dde3d92307a
skosana721 <wonganiskosana@gmail.com>
2021-03-26 10:25:38 +0200

    created a form with an input and a button

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+        <h2>Todo list</h2>
       </header>
+      <form action="">
+        <input type="text" placeholder='Enter a todo'/>
+        <button>ADD</button>
+      </form>
     </div>
   );
 }
