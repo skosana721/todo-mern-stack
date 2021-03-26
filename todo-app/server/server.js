b4e26d1efce5791894788add27105c3e8255ecdb
skosana721 <wonganiskosana@gmail.com>
2021-03-26 14:46:00 +0200

    placed the useUnifiedTopology and useNewUrlParser in a single object instead of two separate objects

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -4,11 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useUnifiedTopology: true },
-  { useNewUrlParser: true }
-);
-console.log(dotenv.config());
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
