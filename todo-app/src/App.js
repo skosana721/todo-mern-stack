8436848f6eca5ba63f3069af71d7853b3d4a706d
skosana721 <wonganiskosana@gmail.com>
2021-03-26 10:30:00 +0200

    imported useState from react and created state for todoList and added an onChange for the input and a value as todoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState("");
   return (
     <div className="App">
       <header className="App-header">
         <h2>Todo list</h2>
       </header>
-      <form action="">
-        <input type="text" placeholder='Enter a todo'/>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter a todo"
+          onChange={(e) => setTodoList(e.target.value)}
+          value={todoList}
+        />
         <button>ADD</button>
       </form>
     </div>
