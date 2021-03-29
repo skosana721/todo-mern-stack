3f30a4b46a22fb4dea825534659c966f5fc1b1c4
skosana721 <wonganiskosana@gmail.com>
2021-03-29 09:39:32 +0200

    add express built in bodyparser

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = require("./route/api/todo");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -8,6 +9,9 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
