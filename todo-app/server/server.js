cef880dceb73abec2daa9a1a1f12924523d99288
skosana721 <wonganiskosana@gmail.com>
2021-03-30 10:38:31 +0200

    imported cors and used it as a middleware

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const { todoListApi } = require("./route/api/todo");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 todoListApi(app);
 
 const PORT = 4000;
