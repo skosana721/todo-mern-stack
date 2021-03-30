de5f2cb363da20d09beb0a6f07edb99ab2fc0705
skosana721 <wonganiskosana@gmail.com>
2021-03-30 10:07:07 +0200

    change imported from router to todoListApi and use the function todoListApi and passed as a argument

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const router = require("./route/api/todo");
+const { todoListApi } = require("./route/api/todo");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
+todoListApi(app);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
