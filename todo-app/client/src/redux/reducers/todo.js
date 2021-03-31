57875414548449fc416641654f67b07d05bc86e7
skosana721 <wonganiskosana@gmail.com>
2021-03-31 13:47:10 +0200

    created case for GET_TODOS and ADD_TODOs

--- todo-app/client/src/redux/reducers/todo.js
+++ todo-app/client/src/redux/reducers/todo.js
@@ -1,8 +1,17 @@
+import { ADD_TODOS, GET_TODOS } from "../actionTypes/todos";
+
 const initialState = {
   todoList: [],
 };
 export const todoReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_TODOS:
+      return { ...state, todoList: action.payload };
+    case ADD_TODOS:
+      return {
+        ...state,
+        todoList: [...state.todoList, action.payload],
+      };
     default:
       return state;
   }
