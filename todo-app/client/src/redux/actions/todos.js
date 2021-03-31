1c93c410dd94f1da7db28e4989b0dc79e1c03e1b
skosana721 <wonganiskosana@gmail.com>
2021-03-31 13:49:12 +0200

    created an for addTodos which returns axois.post function with path then dispatch an actionType of ADD_TODOS and payload of res.data

--- todo-app/client/src/redux/actions/todos.js
+++ todo-app/client/src/redux/actions/todos.js
@@ -1,11 +1,23 @@
 import axios from "axios";
-import { GET_TODOS } from "../actionTypes/todos";
+import { ADD_TODOS, GET_TODOS } from "../actionTypes/todos";
 
 export const getTodos = () => {
   return (dispatch) => {
     axios.get("http://localhost:4000/api/todo").then((res) =>
       dispatch({
         type: GET_TODOS,
+        payload: res.data,
+      })
+    );
+  };
+};
+
+export const addTodos = (todo) => {
+  console.log("todo", todo);
+  return (dispatch) => {
+    axios.post("http://localhost:4000/api/todo", { todo }).then((res) =>
+      dispatch({
+        type: ADD_TODOS,
         payload: res.data,
       })
     );
