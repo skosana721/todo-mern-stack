5fd7a2fae693a861a6af1b11c9b96d053c70ed8e
skosana721 <wonganiskosana@gmail.com>
2021-04-01 12:16:13 +0200

    imported aciton for closeModal and dipatch it on click of the icon close

--- todo-app/client/src/component/FormModel.js
+++ todo-app/client/src/component/FormModel.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Form from "./Form";
+import { useDispatch } from "react-redux";
+import { closeModal } from "../redux/actions/todos";
 
 function FormModel() {
+  const dispatch = useDispatch();
   return (
     <div className="modal">
-      <i class="far fa-times-circle"></i>
+      <i class="far fa-times-circle" onClick={() => dispatch(closeModal())}></i>
       <Form />
     </div>
   );
