4dba396f757164cef78e7e91f6c83b8eb06049f4
skosana721 <wonganiskosana@gmail.com>
2021-04-01 12:18:40 +0200

    added actionTypes for OPEN_MODAL and CLOSE_MODAL

--- todo-app/client/src/redux/actionTypes/todos.js
+++ todo-app/client/src/redux/actionTypes/todos.js
@@ -1,3 +1,5 @@
 export const GET_TODOS = "GET_TODOS";
 export const ADD_TODOS = "ADD_TODOS";
 export const DELETE_TODOS = "DELETE_TODOS";
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
