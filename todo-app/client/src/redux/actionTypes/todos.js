e9a8c28ddbbf5f1707bb390e958a6347036ba43f
skosana721 <wonganiskosana@gmail.com>
2021-05-04 13:14:34 +0200

    created an actiontype for  EDIT_TODOS

--- todo-app/client/src/redux/actionTypes/todos.js
+++ todo-app/client/src/redux/actionTypes/todos.js
@@ -1,5 +1,6 @@
 export const GET_TODOS = "GET_TODOS";
 export const ADD_TODOS = "ADD_TODOS";
 export const DELETE_TODOS = "DELETE_TODOS";
+export const EDIT_TODOS = "EDIT_TODOS";
 export const OPEN_MODAL = "OPEN_MODAL";
 export const CLOSE_MODAL = "CLOSE_MODAL";
